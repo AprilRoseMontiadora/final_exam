4129a36755cd03484859bd85e6fa3e91e5c6e445
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2023-01-03 23:23:14 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,9 @@
-import java.util.*;
-
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+class Student {
+	private String name;
+	public Student(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return name;
 	}
 }
