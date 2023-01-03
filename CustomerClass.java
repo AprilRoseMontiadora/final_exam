9d5a34a77dcfae299768708b42b4de0995162c93
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2023-01-03 22:48:09 +0800

    Update CustomerClass.java

--- CustomerClass.java
+++ CustomerClass.java
@@ -1,3 +1,17 @@
-public class CustomerClass
-{
+abstract class Customer {
+	protected String name;
+	protected double amount;
+
+	public Customer(String name, double amount) {
+		this.name = name;
+		this.amount = amount;
+	}
+	public String getName(){
+		return name;
+
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+ 	public abstract double calculateBill();
 }
